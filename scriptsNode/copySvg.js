293ba861c7ad29b07dd1c0b430839b4b8c0bf841
ilia.brykin <brykin.ilia@gmail.com>
2024-12-13 18:18:18 +0100

    Refactoring and code cleanup

--- scriptsNode/copySvg.js
+++ scriptsNode/copySvg.js
@@ -4,27 +4,27 @@ const _ = require("lodash");
 
 const PATHS_SVG = [
   {
-    "node_modules": "./node_modules/bootstrap-icons/icons/",
+    "src": path.join("node_modules", "bootstrap-icons", "icons"),
     "dist": path.join("dist", "svg", "bootstrap"),
   },
   {
-    "node_modules": "./node_modules/flag-icons/flags/4x3/",
+    "src": path.join("node_modules", "flag-icons", "flags", "4x3"),
     "dist": path.join("dist", "svg", "flags", "4x3"),
   },
   {
-    "node_modules": "./node_modules/flag-icons/flags/1x1/",
+    "src": path.join("node_modules", "flag-icons", "flags", "1x1"),
     "dist": path.join("dist", "svg", "flags", "1x1"),
   },
 ];
 
 PATHS_SVG.forEach(item => {
   copyCurrentSVGs({
-    pathNodeModules: item.node_modules,
+    pathSrc: item.src,
     pathDist: item.dist,
   })
 });
 
-function copyCurrentSVGs({ pathNodeModules, pathDist }) {
+function copyCurrentSVGs({ pathSrc, pathDist }) {
   if (fs.existsSync(pathDist)) {
     fs.rmSync(pathDist, { recursive: true });
   }
@@ -32,19 +32,19 @@ function copyCurrentSVGs({ pathNodeModules, pathDist }) {
     recursive: true
   });
 
-  fs.readdir(pathNodeModules, (err, files) => {
+  fs.readdir(pathSrc, (err, files) => {
     files.forEach(fileName => {
-      copyCurrentSVG({ fileName, pathNodeModules, pathDist });
+      copyCurrentSVG({ fileName, pathSrc, pathDist });
     });
   });
 }
 
-function copyCurrentSVG({ fileName, pathNodeModules, pathDist }) {
+function copyCurrentSVG({ fileName, pathSrc, pathDist }) {
   if (!_.endsWith(fileName, ".svg")) {
     return;
   }
 
-  const SVG_FILE_PATH = path.join(pathNodeModules, fileName);
+  const SVG_FILE_PATH = path.join(pathSrc, fileName);
   const SVG_FILE_PATH_NEW = path.join(pathDist, fileName);
   fs.copyFile(SVG_FILE_PATH, SVG_FILE_PATH_NEW, (err) => {
     if (err) {
